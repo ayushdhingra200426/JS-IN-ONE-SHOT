2d11024e2c6211b5b121c1ea12adb44c6359450a
AyushDhinga <ayushdhingra262626@gmail.com>
2025-10-28 14:25:05 +0530

    Updated files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello");
+let age=18;
+var name='ayush';
+const no=100;
+var name='dhingra'
+age=20;
+console.table([age,name,no]);
+// const once declared cannot be redeclared and updated 
+// var once declared can be redeclared and updated
+// const once declared can be updated but can't be redeclared
+// avoid var because of block scope and functional scope problems
